deb4b862746ed2b6d625ee968258330e9d46641a
Dania Diar <daniadeyar@gmail.com>
2022-08-06 23:33:36 +0300

    completed assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,15 +1,15 @@
-const sinon = require('sinon');
+const sinon = require("sinon");
 
 describe("index.js", () => {
   let input;
 
-  beforeEach(function() {
-    input = document.getElementById('button');
-    sinon.spy(input, 'addEventListener');
-  })
-
+  beforeEach(function () {
+    input = document.getElementById("button");
+    sinon.spy(input, "addEventListener");
+  });
+  console.log(input);
   it("binds an event listener in addingEventListener()", () => {
     addingEventListener();
     expect(input.addEventListener.called).to.be.true;
-  })
-})
+  });
+});
